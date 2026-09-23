830807f4187f326f25395a51f016d9bbfc080087
Wouter Verweirder <wouter.verweirder@gmail.com>
2026-09-23 15:18:27 +0200

    version bump

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __all__ = [
 
 __author__ = """Wouter Verweirder"""
 __email__ = "wouter.verweirder@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .src.penplotter.nodes import NODE_CLASS_MAPPINGS
 from .src.penplotter.nodes import NODE_DISPLAY_NAME_MAPPINGS
